0bede21c7e39fed7fdcdc53592190c687a4de660
Chiyuan Zhang <pluskid@gmail.com>
2014-10-27 17:09:17 -0400

    handle regularization in optimization

--- src/solvers.jl
+++ src/solvers.jl
@@ -20,28 +20,53 @@ function init(net::Net)
   end
 end
 
+function forward(net::Net)
+  obj_val = 0.0
+
+  for i = 1:length(net.layers)
+    forward(net.sys, net.states[i], net.blobs_forward[i])
+
+    if isa(net.layers[i], LossLayer)
+      obj_val += net.states[i].blobs[1].data[1]
+    end
+
+    # handle regularization
+    if :parameters ∈ names(net.states[i])
+      for param in net.states[i].parameters
+        obj_val += forward(net.sys, param.regularizer, param.blob)
+      end
+    end
+  end
+
+  return obj_val
+end
+
+function backward(net::Net)
+  for i = length(net.layers):-1:1
+    backward(net.sys, net.states[i], net.blobs_forward[i], net.blobs_backward[i])
+
+    # handle regularization
+    if :parameters ∈ names(net.states[i])
+      for param in net.states[i].parameters
+        backward(net.sys, param.regularizer, param.blob, param.gradient)
+      end
+    end
+  end
+end
+
 function solver_task(net::Net{CPU})
   function solver_loop()
     init(net)
 
     for iter = 1:net.sys.max_iter
-      for i = 1:length(net.layers)
-        forward(net.sys, net.states[i], net.blobs_forward[i])
-      end
-
-      for i = length(net.layers):-1:1
-        backward(net.sys, net.states[i], net.blobs_forward[i], net.blobs_backward[i])
-      end
+      obj_val = forward(net)
+      backward(net)
 
       # switch to the actual solver co-routine
       produce(iter)
 
       if iter % 100 == 0
-        for i = 1:length(net.layers)
-          if isa(net.layers[i], LossLayer)
-            @printf("%06d %s = %f\n", iter, net.layers[i].name, net.states[i].blobs[1].data[1])
-          end
-        end
+        @printf("%06d objective function = %f\n", iter, obj_val)
       end
     end
 
