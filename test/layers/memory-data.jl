86599bcd6891ec028beeba2398a93d877b40d660
Chiyuan Zhang <pluskid@gmail.com>
2014-10-30 23:54:24 -0400

    test memory data layer

--- test/layers/memory-data.jl
+++ test/layers/memory-data.jl
@@ -1,6 +1,4 @@
-using HDF5
-
-function test_hdf5_data_layer()
+function test_memory_data_layer()
   println("-- Testing HDF5 Data Layer...")
 
   ############################################################
@@ -10,32 +8,15 @@ function test_hdf5_data_layer()
   data_dim = (2,3,4)
   eps = 1e-15
 
-  data_all = [rand(data_dim..., x) for x in [5 1 2]]
-  h5fn_all = [string(tempname(), ".hdf5") for x in 1:length(data_all)]
-
-  for i = 1:length(data_all)
-    h5 = h5open(h5fn_all[i], "w")
-    h5["data"] = data_all[i]
-    close(h5)
-  end
-
-  source_fn = string(tempname(), ".txt")
-  open(source_fn, "w") do s
-    for fn in h5fn_all
-      println(s, fn)
-    end
-  end
+  data = rand(data_dim..., 100)
 
   ############################################################
   # Setup
   ############################################################
 
   # batch size is determined by
-  layer = HDF5DataLayer(; source = source_fn, tops = String["data"], batch_size=batch_size)
+  layer = MemoryDataLayer(; data = data, tops = String["data"], batch_size=batch_size)
   state = setup(sys_cudnn, layer, Blob[])
-
-  data = cat(4, data_all...)
-  data = cat(4, data, data)
 
   data_idx = map(x->1:x, data_dim)
   layer_data = Array(eltype(data), tuple(data_dim..., batch_size))
@@ -46,4 +27,5 @@ function test_hdf5_data_layer()
   end
 end
 
-test_hdf5_data_layer()
+test_memory_data_layer()
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,6 @@ sys_cudnn = System(backend_cudnn, 0.0005, 0.01, 0.9, 5000)
 init(sys_cudnn)
 
 include("layers/hdf5-data.jl")
+include("layers/memory-data.jl")
 
 shutdown(sys_cudnn)
