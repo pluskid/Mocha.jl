b4edcb4f7c2e42501423d590a6dfc398e4365c04
Chiyuan Zhang <pluskid@gmail.com>
2014-11-01 16:04:06 -0400

    L2 regularizer for GPU

--- examples/test.jl
+++ examples/test.jl
@@ -9,25 +9,24 @@ X = rand(N, M)
 W = rand(M, P)
 B = rand(1, P)
 
-Y = ((X*W .+ B)).^2
-m = mean(Y)
-Y[Y .> m] = 2
-Y[Y .<= m] = 1
+Y = (X*W .+ B)
+Y = Y + 0.01*randn(size(Y))
 
 ############################################################
 # Define network
 ############################################################
-sys = System(CPUBackend(), 0.0005, 0.01, 0.9, 5000)
+sys = System(CuDNNBackend(), 0.0005, 0.01, 0.9, 5000)
+init(sys)
 
 data_layer = MemoryDataLayer(; batch_size=100, data=Array[X,Y])
-weight_layer = InnerProductLayer(; output_dim=M, tops=String["pred"], bottoms=String["data"], neuron = Neurons.Sigmoid())
-weight_layer2 = InnerProductLayer(; output_dim=2, tops=String["pred2"], bottoms=String["pred"], neuron = Neurons.Identity())
-loss_layer = SoftmaxLossLayer(; bottoms=String["pred2", "label"])
+weight_layer = InnerProductLayer(; output_dim=M, tops=String["pred"], bottoms=String["data"])
+loss_layer = SquareLossLayer(; bottoms=String["pred", "label"])
 
-net = Net(sys, [loss_layer, weight_layer, weight_layer2, data_layer])
+net = Net(sys, [loss_layer, weight_layer, data_layer])
 
 ############################################################
 # Solve
 ############################################################
 solver = SGD()
 solve(solver, net)
+shutdown(sys)
--- src/regularizers.jl
+++ src/regularizers.jl
@@ -34,8 +34,15 @@ end
 function forward(sys::System{CPUBackend}, regu :: L2Regu, param :: Blob)
   return regu.coefficient * vecnorm(param.data)
 end
-
-function backward(sys::System{CPUBackend}, regu :: L2Regu, param:: Blob, gradient :: Blob)
+function backward(sys::System{CPUBackend}, regu :: L2Regu, param :: Blob, gradient :: Blob)
   gradient.data += regu.coefficient * param.data
 end
 
+function forward(sys::System{CuDNNBackend}, regu :: L2Regu, param :: Blob)
+  return regu.coefficient * CuBLAS.dot(sys.backend.cublas_ctx, length(param),
+      param.ptr, 1, param.ptr, 1)
+end
+function backward(sys::System{CuDNNBackend}, regu :: L2Regu, param :: Blob, gradient :: Blob)
+    CuBLAS.axpy(sys.backend.cublas_ctx, length(param),
+        convert(data_type, regu.coefficient), param.ptr, 1, gradient.ptr, 1)
+end
