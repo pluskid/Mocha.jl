c3fcc0e7612952d98c53d46ab0af83fadf4feefc
Chiyuan Zhang <pluskid@gmail.com>
2014-11-11 21:47:34 -0500

    profiling on mnist

--- examples/mnist/mnist.jl
+++ examples/mnist/mnist.jl
@@ -29,7 +29,7 @@ init(sys)
 common_layers = [conv_layer, pool_layer, conv2_layer, pool2_layer, fc1_layer, fc2_layer]
 net = Net(sys, [data_layer, common_layers..., loss_layer])
 
-params = SolverParameters(max_iter=10000, regu_coef=0.0005, base_lr=0.01, momentum=0.9,
+params = SolverParameters(max_iter=10, regu_coef=0.0005, base_lr=0.01, momentum=0.9,
     lr_policy=LRPolicy.Inv(0.0001, 0.75))
 solver = SGD(params)
 
@@ -47,6 +47,11 @@ add_coffee_break(solver,
     Snapshot("snapshots", auto_load=true, also_load_solver_state=false), 
     every_n_iter=5000)
 
-solve(solver, net)
+#solve(solver, net)
+Profile.init(int(1e8), 0.001)
+@profile solve(solver, net)
+open("profile.txt", "w") do out
+  Profile.print(out)
+end
 
 shutdown(sys)
