544b78de19724acc56a469dffeffde96208f57b8
Chiyuan Zhang <pluskid@gmail.com>
2014-11-12 01:40:20 -0500

    benchmark of parallel pooling on laptop

--- benchmarks/parallel-pool.jl
+++ benchmarks/parallel-pool.jl
@@ -1,5 +1,20 @@
 using Benchmark
 
+#------------------------------------------------------------
+# On my laptop
+#
+# julia:
+# | Row | Function             | Average   | Relative | Replications |
+# |-----|----------------------|-----------|----------|--------------|
+# | 1   | "pool_single_proc"   | 0.0320213 | 1.0      | 50           |
+# | 2   | "pool_multiple_proc" | 0.067476  | 2.10722  | 50           |
+#
+# julia -p 2:
+# | Row | Function             | Average   | Relative | Replications |
+# |-----|----------------------|-----------|----------|--------------|
+# | 1   | "pool_single_proc"   | 0.0333833 | 1.0      | 50           |
+# | 2   | "pool_multiple_proc" | 4.48791   | 134.436  | 50           |
+
 require("parallel-pool-module")
 
 input = rand(28, 28, 20, 128)
@@ -13,4 +28,5 @@ output2 = pool_multiple_proc(input, :max, kernel_dim, stride_dim)
 pool_single_proc() = pool_single_proc(input, :max, kernel_dim, stride_dim)
 pool_multiple_proc() = pool_multiple_proc(input, :max, kernel_dim, stride_dim)
 
-df = compare([pool_single_proc, pool_multiple_proc], 10)
+df = compare([pool_single_proc, pool_multiple_proc], 50)
+println("$df")
