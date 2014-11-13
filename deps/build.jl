167f0335c154eb1b4eeaa6b798afd5fdedaa7268
Chiyuan Zhang <pluskid@gmail.com>
2014-11-12 23:58:33 -0500

    a proper build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,23 @@
-println("Hello World")
-println(pwd())
+sources = ["im2col.cpp", "pooling.cpp"]
+
+compiler = "g++"
+flags    = ["-fPIC", "-Wall", "-O3", "-shared"]
+libname  = "libmochaext.so"
+openmp   = "-fopenmp"
+
+@osx? begin
+  if !haskey(ENV, "MOCHA_FORCE_OMP")
+    println("OpenMP is currently not officially supported by OS X Clang compiler yet.")
+    println("(see http://clang-omp.github.io/ to install OpenMP clang extension, or")
+    println("install gcc.")
+    println("")
+    println("Disabling OpenMP. To force enable OpenMP, set MOCHA_FORCE_OMP environment")
+    println("variable.")
+    println("")
+    openmp = ""
+  end
+end : nothing
+
+cmd = `$compiler $flags $openmp -o $libname $sources`
+println("Running $cmd")
+run(cmd)
