8e6e932fb36097bca5445082065370baac5829ab
Chiyuan Zhang <pluskid@gmail.com>
2014-11-18 19:23:17 -0500

    float32 and float64 for raw blas test

--- test/utils/blas.jl
+++ test/utils/blas.jl
@@ -1,32 +1,37 @@
-function test_raw_blas()
-  println("-- Testing RawBLAS Utilities")
+function test_raw_blas(T)
+  println("-- Testing RawBLAS{$T} Utilities")
 
   eps = 1e-10
   M, N, K = (108, 6, 36)
-  A = rand(M, K)
-  B = rand(N, K)
+  A = rand(T, M, K)
+  B = rand(T, N, K)
 
-  C = rand(M, N)
-  C2 = rand(M, N)
-  RawBLAS.gemm!('N', 'T', M, N, K, 1.0, A, M, B, N, 0.0, C, M)
-  BLAS.gemm!('N', 'T', 1.0, A, B, 0.0, C2)
+  C = rand(T, M, N)
+  C2 = rand(T, M, N)
+  RawBLAS.gemm!('N', 'T', M, N, K, convert(T,1.0), A, M, B, N, convert(T,0.0), C, M)
+  BLAS.gemm!('N', 'T', convert(T,1.0), A, B, convert(T,0.0), C2)
 
   @test all(abs(C - C2) .< eps)
 
-  A2 = rand(M, 2K)
-  B2 = rand(N, 3K)
-  C = rand(M, N)
+  A2 = rand(T, M, 2K)
+  B2 = rand(T, N, 3K)
+  C = rand(T, M, N)
   C2 = A2[:, 1:K] * B2[:, 1:K]'
-  RawBLAS.gemm!('N', 'T', M, N, K, 1.0, A2, M, B2, N, 0.0, C, M)
+  RawBLAS.gemm!('N', 'T', M, N, K, convert(T,1.0), A2, M, B2, N, convert(T,0.0), C, M)
   @test all(abs(C - C2) .< eps)
 
   C2 = A2[:, K+1:2K] * B2[:, 2K+1:3K]'
-  C = rand(M, 2N)
-  RawBLAS.gemm!('N', 'T', M, N, K, 1.0,
-      convert(Ptr{Float64},A2) + M*K*sizeof(Float64), M,
-      convert(Ptr{Float64},B2) + 2N*K*sizeof(Float64), N,
-      0.0, convert(Ptr{Float64},C) + M*N*sizeof(Float64), M)
+  C = rand(T, M, 2N)
+  RawBLAS.gemm!('N', 'T', M, N, K, convert(T, 1.0),
+      convert(Ptr{T},A2) + M*K*sizeof(T), M,
+      convert(Ptr{T},B2) + 2N*K*sizeof(T), N,
+      convert(T,0.0), convert(Ptr{T},C) + M*N*sizeof(T), M)
   @test all(abs(C2 - C[1:M,N+1:2N]) .< eps)
+end
+
+test_raw_blas() = begin
+  test_raw_blas(Float32)
+  test_raw_blas(Float64)
 end
 
 test_raw_blas()
