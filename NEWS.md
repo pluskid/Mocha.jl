998401f65605e8144d1ff7126b9a1a2ee2c187e1
Chiyuan Zhang <pluskid@gmail.com>
2014-11-27 19:07:47 -0500

    allow CUDA and NATIVE_EXT be disabled by setting env variable to "false"

--- NEWS.md
+++ NEWS.md
@@ -11,7 +11,7 @@
 * Document
   * Developer's Guide
 
-## v0.0.3 2014.11.??
+## v0.0.3 2014.11.27
 
 * Interface
   * IJulia-notebook example
--- src/config.jl
+++ src/config.jl
@@ -6,7 +6,7 @@ info("Configuring Mocha...")
 const use_cuda_key = "MOCHA_USE_CUDA"
 const use_native_extension_key = "MOCHA_USE_NATIVE_EXT"
 
-if haskey(ENV, use_cuda_key)
+if haskey(ENV, use_cuda_key) && lowercase(ENV[use_cuda_key]) != "false"
   info(" * CUDA       enabled ($use_cuda_key environment variable detected)")
   const use_cuda = true
 else
@@ -14,7 +14,7 @@ else
   const use_cuda = false
 end
 
-if haskey(ENV, use_native_extension_key)
+if haskey(ENV, use_native_extension_key) && lowercase(ENV[use_native_extension_key]) != "false"
   info(" * Native Ext enabled ($use_native_extension_key environment variable detected)")
   const use_native_extension = true
 else
