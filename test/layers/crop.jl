a8c564e5e91da1db752c90b9e5cfcc845ba15ee7
Chiyuan Zhang <pluskid@gmail.com>
2014-12-03 13:14:06 -0500

    cover random crop with unit test

--- test/layers/crop.jl
+++ test/layers/crop.jl
@@ -27,9 +27,42 @@ function test_crop_layer(sys::System, do_mirror, T, eps)
 
   shutdown(sys, state)
 end
+function test_crop_layer_random(sys::System, do_mirror, T, eps)
+  println("-- Testing CropLayer{rnd} on $(typeof(sys.backend)){$T} $(do_mirror ? "with mirror" : "")...")
+  input = rand(T, 11, 12, 5, 6)
+  crop_size = (9, 9)
+  input_blob = make_blob(sys.backend, input)
+
+  println("    > Setup")
+  layer = CropLayer(bottoms=[:input], tops=[:output], crop_size=crop_size,
+      random_mirror=do_mirror, random_crop=true)
+  state = setup(sys, layer, Blob[input_blob], Blob[NullBlob()])
+
+  println("    > Forward")
+  forward(sys, state, Blob[input_blob])
+  got_output = zeros(T, size(state.blobs[1]))
+  copy!(got_output, state.blobs[1])
+
+  matched = false
+  for i = 0:size(input,1)-crop_size[1]
+    for j = 0:size(input,2)-crop_size[2]
+      expected_output = input[i+1:i+crop_size[1], j+1:j+crop_size[2],:,:]
+      matched = matched | all(abs(got_output - expected_output) .< eps)
+      if do_mirror
+        matched = matched | all(abs(got_output - flipdim(expected_output,1)) .< eps)
+      end
+    end
+  end
+  @test matched
+
+  shutdown(sys, state)
+end
+
 function test_crop_layer(sys::System, T, eps)
   test_crop_layer(sys, false, T, eps)
   test_crop_layer(sys, true, T, eps)
+  test_crop_layer_random(sys, false, T, eps)
+  test_crop_layer_random(sys, true, T, eps)
 end
 
 function test_crop_layer(sys::System)
