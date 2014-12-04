91c5610be1ed0edfdf76cb53d329de0fe63f5082
Chiyuan Zhang <pluskid@gmail.com>
2014-12-04 11:40:19 -0500

    add process id list to CPUBackend

--- src/backend.jl
+++ src/backend.jl
@@ -7,7 +7,21 @@ end
 function shutdown(backend::Backend)
 end
 
-type CPUBackend <: Backend; end
+type CPUBackend{N} <: Backend
+  pids :: NTuple{N, Int}
+
+  CPUBackend(pids::NTuple{N,Int}) = begin
+    for pid in pids
+      if !in(pid, procs())
+        error("$pid is not a valid process id")
+      end
+    end
+    new(pids)
+  end
+end
+
+CPUBackend() = CPUBackend{1}((myid(),))
+CPUBackend(pids::Vector{Int}) = CPUBackend{length(pids)}(tuple(pids...))
 
 # This is forward declaration to allow some code to compile
 # (especially testing codes) even if CUDA module is completely
