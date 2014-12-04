3c258f91d23aa3ba72e9552d92be4326d28e2175
Chiyuan Zhang <pluskid@gmail.com>
2014-12-03 22:49:45 -0500

    shuffling support for HDF5 data layer

--- src/layers/hdf5-data.jl
+++ src/layers/hdf5-data.jl
@@ -5,6 +5,7 @@ using HDF5
   (source :: String = "", source != ""),
   (batch_size :: Int = 0, batch_size > 0),
   (tops :: Vector{Symbol} = Symbol[:data,:label], length(tops) > 0),
+  shuffle :: Bool = false,
   transformers :: Vector = [],
 )
 
@@ -15,10 +16,11 @@ type HDF5DataLayerState <: LayerState
   trans :: Vector{Vector{DataTransformerState}}
 
   sources        :: Vector{String}
-  dsets          :: Vector{String}
+  dsets          :: Vector{Any}
   curr_source    :: Int
   curr_hdf5_file :: HDF5File
   curr_index     :: Int
+  shuffle_idx    :: Vector{Int}
 
   HDF5DataLayerState(sys::System, layer::HDF5DataLayer) = begin
     state = new(layer)
@@ -28,22 +30,30 @@ type HDF5DataLayerState <: LayerState
     end
     @assert(length(sources) > 0)
     state.sources = sources
-    state.dsets = [string(x) for x in layer.tops]
-
     state.epoch = 0
     state.curr_source = 1
     state.curr_hdf5_file = h5open(sources[1], "r")
+
+    state.dsets = [state.curr_hdf5_file[string(x)] for x in layer.tops]
+    if layer.shuffle
+      # we only use mmap when random shuffle is required, as mmap is slower
+      # than sequential batch read. see hdf5-read benchmark.
+      state.dsets = map(readmmap, state.dsets)
+      state.shuffle_idx = randperm(size(state.dsets[1],4))
+    else
+      state.shuffle_idx = Int[]
+    end
 
     state.blobs = Array(Blob, length(layer.tops))
     state.trans = Array(Vector{DataTransformerState}, length(layer.tops))
     transformers = convert(Vector{(Symbol, DataTransformerType)}, layer.transformers)
     for i = 1:length(state.blobs)
-      dims = size(state.curr_hdf5_file[state.dsets[i]])
+      dims = size(state.dsets[i])
       @assert(length(dims)==4, "HDF5 dataset $(state.dsets[i]) is not 4D tensor")
 
       dims = tuple(dims[1:3]..., layer.batch_size)
 
-      dset = state.curr_hdf5_file[state.dsets[i]]
+      dset = state.dsets[i]
       state.blobs[i] = make_blob(sys.backend, eltype(dset), dims)
 
       state.trans[i] = [setup(sys, convert(DataTransformerType, t), state.blobs[i])
@@ -69,21 +79,32 @@ end
 function forward(sys::System, state::HDF5DataLayerState, inputs::Vector{Blob})
   n_done = 0
   while n_done < state.layer.batch_size
-    n_remain = size(state.curr_hdf5_file[state.dsets[1]])[4] - state.curr_index + 1
+    n_remain = size(state.dsets[1],4) - state.curr_index + 1
     if n_remain == 0
       close(state.curr_hdf5_file)
       state.curr_source = state.curr_source % length(state.sources) + 1
       state.curr_hdf5_file = h5open(state.sources[state.curr_source], "r")
+      state.dsets = [state.curr_hdf5_file[string(x)] for x in state.layer.tops]
+
+      if state.layer.shuffle
+        state.dsets = map(readmmap, state.dsets)
+        state.shuffle_idx = randperm(size(state.dsets[1],4))
+      end
+
       state.curr_index = 1
-      n_remain = size(state.curr_hdf5_file[state.dsets[1]])[4]
+      n_remain = size(state.dsets[1], 4)
     end
 
     n1 = min(state.layer.batch_size-n_done, n_remain)
     if n1 > 0
       for i = 1:length(state.blobs)
         idx = map(x -> 1:x, size(state.blobs[i])[1:3])
-        dset = state.curr_hdf5_file[state.dsets[i]]
-        the_data = dset[idx..., state.curr_index:state.curr_index+n1-1]
+        dset = state.dsets[i]
+        if state.layer.shuffle
+          the_data = dset[idx..., state.shuffle_idx[state.curr_index:state.curr_index+n1-1]]
+        else
+          the_data = dset[idx..., state.curr_index:state.curr_index+n1-1]
+        end
         set_blob_data(the_data, state.blobs[i], n_done+1)
       end
     end
@@ -91,7 +112,7 @@ function forward(sys::System, state::HDF5DataLayerState, inputs::Vector{Blob})
     n_done += n1
 
     # update epoch
-    if state.curr_index > size(state.curr_hdf5_file[state.dsets[1]])[4] &&
+    if state.curr_index > size(state.dsets[1],4) &&
         state.curr_source == length(state.sources)
       state.epoch += 1
     end
