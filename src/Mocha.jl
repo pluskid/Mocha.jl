e81c63eb4ae9d00021383d6a846c8cfb7147b59a
Chiyuan Zhang <pluskid@gmail.com>
2014-12-08 00:02:37 -0500

    update to HDF5.jl 0.4.7

--- src/Mocha.jl
+++ src/Mocha.jl
@@ -15,7 +15,6 @@ include("exception.jl")
 include("utils/blas.jl")
 include("utils/math.jl")
 include("utils/io.jl")
-include("utils/hdf5.jl")
 
 if Config.use_native_extension
   include("utils/im2col-native.jl")
