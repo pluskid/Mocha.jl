4dbc589e192c14616f3458cb834ba1a1ec9fcf69
Chiyuan Zhang <pluskid@gmail.com>
2014-12-19 15:24:54 +0800

    General ND-tensor support for ConcatLayer (CUDA)

--- src/cuda/utils/shifted-copy.jl
+++ src/cuda/utils/shifted-copy.jl
@@ -1,15 +1,23 @@
 export copy_to_shifted!, copy_from_shifted!
 
 function copy_to_shifted!{T}(backend::GPUBackend, dst::CuTensorBlob{T},
-    src::CuTensorBlob{T}, shifts::Vector{Int})
+    src::CuTensorBlob{T}, shift::NTuple{2,Int})
 
-  width, height, channels, num = size(src)
-  width2, height2, channels2, num2 = size(dst)
-  chann_num = channels*num
+  dims = size(src)
+  dims2 = size(dst)
 
-  x_block = int(ceil(float(chann_num)/CUDA.THREADS_PER_BLOCK_X))
-  y_block = int(ceil(float(width)/CUDA.THREADS_PER_BLOCK_Y))
-  z_block = int(ceil(float(height)/CUDA.THREADS_PER_BLOCK_Z))
+  shift_dim, shift_amount = shift
+
+  dim_x  = prod(dims[1:shift_dim-1])
+  @assert dim_x == prod(dims2[1:shift_dim-1])
+  dim_y  = dims[shift_dim]
+  dim_y2 = dims2[shift_dim]
+  dim_z  = prod(dims[shift_dim+1:end])
+  @assert dim_z == prod(dims2[shift_dim+1:end])
+
+  x_block = int(ceil(float(dim_x)/CUDA.THREADS_PER_BLOCK_X))
+  y_block = int(ceil(float(dim_y)/CUDA.THREADS_PER_BLOCK_Y))
+  z_block = int(ceil(float(dim_z)/CUDA.THREADS_PER_BLOCK_Z))
 
   if T == Float32
     kernel = backend.mocha.copy_to_shifted_float
@@ -21,21 +29,27 @@ function copy_to_shifted!{T}(backend::GPUBackend, dst::CuTensorBlob{T},
 
   CUDA.launch(kernel, (x_block,y_block,z_block),
       (CUDA.THREADS_PER_BLOCK_X,CUDA.THREADS_PER_BLOCK_Y,CUDA.THREADS_PER_BLOCK_Z),
-      (dst.ptr.p,src.ptr.p,width,height,channels,num,
-      shifts[1],shifts[2],shifts[3],shifts[4],
-      width2,height2,channels2))
+      (dst.ptr.p,src.ptr.p,dim_x,dim_y,dim_z,dim_y2,shift_amount))
 end
 
 function copy_from_shifted!{T}(backend::GPUBackend, dst::CuTensorBlob{T},
-    src::CuTensorBlob{T}, shifts::Vector{Int})
+    src::CuTensorBlob{T}, shift::NTuple{2,Int})
 
-  width, height, channels, num = size(dst)
-  width2, height2, channels2, num2 = size(src)
-  chann_num = channels*num
+  dims = size(dst)
+  dims2 = size(src)
 
-  x_block = int(ceil(float(chann_num)/CUDA.THREADS_PER_BLOCK_X))
-  y_block = int(ceil(float(width)/CUDA.THREADS_PER_BLOCK_Y))
-  z_block = int(ceil(float(height)/CUDA.THREADS_PER_BLOCK_Z))
+  shift_dim, shift_amount = shift
+
+  dim_x  = prod(dims[1:shift_dim-1])
+  @assert dim_x == prod(dims2[1:shift_dim-1])
+  dim_y  = dims[shift_dim]
+  dim_y2 = dims2[shift_dim]
+  dim_z  = prod(dims[shift_dim+1:end])
+  @assert dim_z == prod(dims2[shift_dim+1:end])
+
+  x_block = int(ceil(float(dim_x)/CUDA.THREADS_PER_BLOCK_X))
+  y_block = int(ceil(float(dim_y)/CUDA.THREADS_PER_BLOCK_Y))
+  z_block = int(ceil(float(dim_z)/CUDA.THREADS_PER_BLOCK_Z))
 
   if T == Float32
     kernel = backend.mocha.copy_from_shifted_float
@@ -47,7 +61,5 @@ function copy_from_shifted!{T}(backend::GPUBackend, dst::CuTensorBlob{T},
 
   CUDA.launch(kernel, (x_block,y_block,z_block),
       (CUDA.THREADS_PER_BLOCK_X,CUDA.THREADS_PER_BLOCK_Y,CUDA.THREADS_PER_BLOCK_Z),
-      (dst.ptr.p,src.ptr.p,width,height,channels,num,
-      shifts[1],shifts[2],shifts[3],shifts[4],
-      width2,height2,channels2))
+      (dst.ptr.p,src.ptr.p,dim_x,dim_y,dim_z,dim_y2,shift_amount))
 end
