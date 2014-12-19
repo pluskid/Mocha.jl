5fa3a4ecff203c9ceff8c663552f3d05bf8d853a
Chiyuan Zhang <pluskid@gmail.com>
2014-12-19 22:10:08 +0800

    argmax layer ND-tensor

--- src/utils/tensor.jl
+++ src/utils/tensor.jl
@@ -4,9 +4,9 @@ export split_dims
 #   (dim_pre, dim_mid, dim_post)
 function split_dims{T}(tensor::T, dim::Int)
   dims = size(tensor)
-  dim_pre  = prod(dims[1:dim-1])
-  dim_mid  = dims[dim]
-  dim_post = prod(dims[dim+1:end])
+  dim_pre  ::Int = prod(dims[1:dim-1])
+  dim_mid  ::Int = dims[dim]
+  dim_post ::Int = prod(dims[dim+1:end])
 
   (dim_pre, dim_mid, dim_post)
 end
