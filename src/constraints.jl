1de4c53c9e76ffe7be22fec70520f3e35b3a6660
Chiyuan Zhang <pluskid@gmail.com>
2014-12-20 14:25:04 +0800

    rename coefficient to threshold in norm constraints

--- src/constraints.jl
+++ src/constraints.jl
@@ -4,16 +4,16 @@ export constrain!
 abstract Constraint
 
 immutable NoCons <: Constraint
-  coefficient :: FloatingPoint # not used, just for consistent API
+  threshold  :: FloatingPoint  # not used, just for consistent API
   every_n_iter :: Int          # also not used
 end
 NoCons() = NoCons(0.0, 0)
 
 immutable L2Cons <: Constraint
-  coefficient :: FloatingPoint
+  threshold    :: FloatingPoint
   every_n_iter :: Int
 end
-L2Cons(coefficient) = L2Cons(coefficient, 1)
+L2Cons(threshold) = L2Cons(threshold, 1)
 
 ############################################################
 # No constraint
@@ -27,14 +27,14 @@ end
 ############################################################
 
 function apply_l2_cons!{T <: FloatingPoint}(backend::CPUBackend, blob::CPUBlob{T},
-                                            coef::FloatingPoint, ninputs::Int, nunits::Int)
+                                            threshold::FloatingPoint, ninputs::Int, nunits::Int)
   param = reshape(blob.data, (ninputs, nunits))
   # we constrain each column vector
   for i = 1:nunits
     # compute norm and scale using blas
     norm = vecnorm(param[:, i])
-    if norm > coef
-      scale_factor =  (1. / norm) * coef
+    if norm > threshold
+      scale_factor =  (1. / norm) * threshold
       offset = sizeof(T) * (i-1) * ninputs
       BLAS.scal!(ninputs, convert(T, scale_factor), convert(Ptr{T}, param) + offset, 1)
     end
@@ -45,8 +45,8 @@ end
 # it is a bit ugly but was the easiest way to implement it for now
 function constrain!(backend :: Backend, cons :: L2Cons, param :: Blob)
   if ndims(param) == 1 # a bias blob
-    apply_l2_cons!(backend, param, cons.coefficient, length(param), 1)
+    apply_l2_cons!(backend, param, cons.threshold, length(param), 1)
   else # general weights
-    apply_l2_cons!(backend, param, cons.coefficient, get_fea_size(param), get_num(param))
+    apply_l2_cons!(backend, param, cons.threshold, get_fea_size(param), get_num(param))
   end
 end
