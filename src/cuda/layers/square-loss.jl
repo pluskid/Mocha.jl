743e4e0741579e63567a83c4717e22fd282fc602
Chiyuan Zhang <pluskid@gmail.com>
2015-01-19 23:55:33 +0800

    make SquareLossLayer also a statistics layer

--- src/cuda/layers/square-loss.jl
+++ src/cuda/layers/square-loss.jl
@@ -17,6 +17,10 @@ function forward(backend::GPUBackend, state::SquareLossLayerState, inputs::Vecto
   copy!(state.pred_copy, pred)
   CuBLAS.axpy(backend.cublas_ctx, n, convert(data_type, -1), label.ptr, 1, state.pred_copy.ptr, 1)
   state.loss = 0.5/get_num(pred)*CuBLAS.dot(backend.cublas_ctx, data_type, n, state.pred_copy.ptr, 1, state.pred_copy.ptr, 1)
+
+  # accumulate statistics
+  state.loss_accum = (state.loss_accum*state.n_accum + state.loss*get_num(pred)) / (state.n_accum+get_num(pred))
+  state.n_accum += get_num(pred)
 end
 
 function backward(backend::GPUBackend, state::SquareLossLayerState, inputs::Vector{Blob}, diffs::Vector{Blob})
--- src/layers/square-loss.jl
+++ src/layers/square-loss.jl
@@ -11,11 +11,15 @@
   has_loss  => true,
   can_do_bp => true,
   is_sink   => true,
+  has_stats => true,
 )
 
 type SquareLossLayerState{T} <: LayerState
-  layer :: SquareLossLayer
-  loss  :: T
+  layer      :: SquareLossLayer
+  loss       :: T
+
+  loss_accum :: T
+  n_accum    :: Int
 
   # a helper blob used to compute the loss without destroying
   # the pred results passed up
@@ -26,11 +30,24 @@ function setup(backend::Backend, layer::SquareLossLayer, inputs::Vector{Blob}, d
   data_type = eltype(inputs[1])
   pred_copy = make_blob(backend, data_type, size(inputs[1])...)
 
-  state = SquareLossLayerState(layer, convert(data_type, 0), pred_copy)
+  state = SquareLossLayerState(layer, zero(data_type), zero(data_type), 0, pred_copy)
   return state
 end
 function shutdown(backend::Backend, state::SquareLossLayerState)
   destroy(state.pred_copy)
+end
+
+function reset_statistics(state::SquareLossLayerState)
+  state.n_accum = 0
+  state.loss_accum = zero(typeof(state.loss_accum))
+end
+function dump_statistics(storage, state::SquareLossLayerState, show::Bool)
+  update_statistics(storage, "$(state.layer.name)-square-loss", state.loss_accum)
+
+  if show
+    loss = @sprintf("%.4f", state.loss_accum)
+    @info("  Square-loss (avg over $(state.n_accum)) = $loss")
+  end
 end
 
 function forward(backend::CPUBackend, state::SquareLossLayerState, inputs::Vector{Blob})
@@ -43,6 +60,10 @@ function forward(backend::CPUBackend, state::SquareLossLayerState, inputs::Vecto
   copy!(state.pred_copy, pred)
   BLAS.axpy!(n, convert(data_type, -1), label.data, 1, state.pred_copy.data, 1)
   state.loss = 0.5/get_num(pred)*BLAS.dot(state.pred_copy.data, state.pred_copy.data)
+
+  # accumulate statistics
+  state.loss_accum = (state.loss_accum*state.n_accum + state.loss*get_num(pred)) / (state.n_accum+get_num(pred))
+  state.n_accum += get_num(pred)
 end
 
 function backward(backend::CPUBackend, state::SquareLossLayerState, inputs::Vector{Blob}, diffs::Vector{Blob})
