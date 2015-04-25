1aea07b97cce33a0220e65064b1dd7c002cf46a9
Brett <brett.israelsen@gmail.com>
2015-04-25 14:51:55 -0600

    Improved to allow for parameters

--- src/coffee/training-summary.jl
+++ src/coffee/training-summary.jl
@@ -1,11 +1,51 @@
 export TrainingSummary
 
 type TrainingSummary <: Coffee
+  dispIter :: Bool
+  dispObj_val :: Bool
+  dispLR :: Bool
+  dispMom:: Bool
+  
+  #Default Constructor
+  #Put the "string" parameter to make the rest be optional
+  function TrainingSummary(string="dontuseme";showIter=true,showObj_val=true,showLR=false,showMom=false)
+    newTrainingSummary = new(showIter,showObj_val,showLR,showMom)
+
+    return newTrainingSummary
+  end
+
+  TrainingSummary() = new(true,true,false,false)
+
 end
 
-function enjoy(lounge::CoffeeLounge, ::TrainingSummary, ::Net, state::SolverState)
+function enjoy(lounge::CoffeeLounge, coffee::TrainingSummary, ::Net, state::SolverState)
   # we do not report objective value at iteration 0 because it has not been computed yet
-  summary = @sprintf("%06d :: TRAIN obj-val = %.8f :: LR = %8f :: Mom = %8f", state.iter, state.obj_val, state.learning_rate, state.momentum)
+  if coffee.dispIter
+    txtIter = @sprintf("ITER = %06d",state.iter)
+  else
+    txtIter = ""
+  end
+
+  if coffee.dispObj_val
+    txtObj_val = @sprintf(":: TRAIN obj-val = %.8f",state.obj_val)
+  else
+    txtObj_val = ""
+  end
+
+  if coffee.dispLR
+    txtLR = @sprintf(":: LR = %.8f",state.learning_rate)
+  else
+    txtLR = ""
+  end
+
+  if coffee.dispMom
+    txtMom = @sprintf(":: MOM = %.8f",state.momentum)
+  else
+    txtMom = ""
+  end
+
+  summary = string(txtIter,txtObj_val,txtLR,txtMom)
+  
   @info(summary)
 
   update_statistics(lounge, "obj-val", state.obj_val)
