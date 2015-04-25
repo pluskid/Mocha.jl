63c65074ae482d27048324e64de10c2fe00b8a67
Brett <brett.israelsen@gmail.com>
2015-04-25 13:50:01 -0600

    Simple modification to display LR and Momentum

--- src/coffee/training-summary.jl
+++ src/coffee/training-summary.jl
@@ -5,7 +5,7 @@ end
 
 function enjoy(lounge::CoffeeLounge, ::TrainingSummary, ::Net, state::SolverState)
   # we do not report objective value at iteration 0 because it has not been computed yet
-  summary = @sprintf("%06d :: TRAIN obj-val = %.8f", state.iter, state.obj_val)
+  summary = @sprintf("%06d :: TRAIN obj-val = %.8f :: LR = %8f :: Mom = %8f", state.iter, state.obj_val, state.learning_rate, state.momentum)
   @info(summary)
 
   update_statistics(lounge, "obj-val", state.obj_val)
