b12e10bfa32d45807fa18107909cdaf34726c54a
Chiyuan Zhang <pluskid@gmail.com>
2015-06-22 22:05:23 -0400

    improve mnist example script

--- examples/mnist/mnist.jl
+++ examples/mnist/mnist.jl
@@ -1,12 +1,17 @@
-#ENV["MOCHA_USE_NATIVE_EXT"] = "true"
-#ENV["OMP_NUM_THREADS"] = 1
-#blas_set_num_threads(1)
-ENV["MOCHA_USE_CUDA"] = "true"
+use_gpu = true
+
+if use_gpu
+  ENV["MOCHA_USE_CUDA"] = "true"
+else
+  ENV["MOCHA_USE_NATIVE_EXT"] = "true"
+  ENV["OMP_NUM_THREADS"] = 1
+  blas_set_num_threads(1)
+end
 
 using Mocha
 srand(12345678)
 
-data_layer  = HDF5DataLayer(name="train-data", source="data/train.txt", batch_size=64, shuffle=@windows ? false : true)
+data_layer  = AsyncHDF5DataLayer(name="train-data", source="data/train.txt", batch_size=64, shuffle=true)
 conv_layer  = ConvolutionLayer(name="conv1", n_filter=20, kernel=(5,5), bottoms=[:data], tops=[:conv])
 pool_layer  = PoolingLayer(name="pool1", kernel=(2,2), stride=(2,2), bottoms=[:conv], tops=[:pool])
 conv2_layer = ConvolutionLayer(name="conv2", n_filter=50, kernel=(5,5), bottoms=[:pool], tops=[:conv2])
@@ -15,13 +20,13 @@ fc1_layer   = InnerProductLayer(name="ip1", output_dim=500, neuron=Neurons.ReLU(
 fc2_layer   = InnerProductLayer(name="ip2", output_dim=10, bottoms=[:ip1], tops=[:ip2])
 loss_layer  = SoftmaxLossLayer(name="loss", bottoms=[:ip2,:label])
 
-backend = GPUBackend()
+backend = use_gpu ? GPUBackend() : CPUBackend()
 init(backend)
 
 common_layers = [conv_layer, pool_layer, conv2_layer, pool2_layer, fc1_layer, fc2_layer]
 net = Net("MNIST-train", backend, [data_layer, common_layers..., loss_layer])
 
-exp_dir = "snapshots"
+exp_dir = "snapshots$(use_gpu ? "-gpu" : "-cpu")"
 
 params = SolverParameters(max_iter=10000, regu_coef=0.0005,
     mom_policy=MomPolicy.Fixed(0.9),
