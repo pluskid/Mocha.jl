d5d4f05dac6671a09de24df848576fb54d2ee58f
Chiyuan Zhang <pluskid@gmail.com>
2015-09-20 22:16:07 -0400

    fix #96, in-place dropout layer cause cuDNN pooling layer bug

--- src/cuda/layers/dropout.jl
+++ src/cuda/layers/dropout.jl
@@ -13,14 +13,26 @@ function setup_etc(backend::GPUBackend, layer::DropoutLayer, inputs::Vector{Blob
   x_block = round(Int64, ceil(float64(len)/CUDA.THREADS_PER_BLOCK_X))
   CUDA.launch(kernel, x_block, CUDA.THREADS_PER_BLOCK_X, (cuda_rand_states, len))
 
-  return cuda_rand_states
+  # hold copy of input blob, we will restore the inputs after backward computing
+  # this is because when used with the cuDNN pooling layer, the in-place modification
+  # of the pooling layer output will cause error in computed gradient due to unknown
+  # implementation of the pooling backward. So as a workaround, we restore the input
+  # after finishing dropout backward computing. See #96 on github for more details.
+  input_copy = make_blob(backend, eltype(inputs[1]), size(inputs[1]))
+
+  return (cuda_rand_states, input_copy)
 end
 
 function destroy_etc(backend::GPUBackend, state::DropoutLayerState)
-  CUDA.free(state.etc)
+  cuda_rand_states, input_copy = state.etc
+  CUDA.free(cuda_rand_states)
+  CUDA.free(input_copy)
 end
 
 function forward(backend::GPUBackend, state::DropoutLayerState, inputs::Vector{Blob})
+  # make copy of input blob
+  copy!(state.etc[2], inputs[1])
+
   len = length(inputs[1])
   x_block = round(Int64, ceil(float64(len)/CUDA.THREADS_PER_BLOCK_X))
   data_type = eltype(inputs[1])
@@ -31,7 +43,7 @@ function forward(backend::GPUBackend, state::DropoutLayerState, inputs::Vector{B
   end
 
   CUDA.launch(kernel, x_block, CUDA.THREADS_PER_BLOCK_X,
-      (state.etc, length(inputs[1]), inputs[1].ptr.p,
+      (state.etc[1], length(inputs[1]), inputs[1].ptr.p,
       state.rand_vals.ptr.p, state.ratio, state.scale))
 end
 
@@ -47,7 +59,10 @@ function backward(backend::GPUBackend, state::DropoutLayerState, inputs::Vector{
     end
 
     CUDA.launch(kernel, x_block, CUDA.THREADS_PER_BLOCK_X,
-        (state.etc, length(inputs[1]), diffs[1].ptr.p,
+        (state.etc[1], length(inputs[1]), diffs[1].ptr.p,
         state.rand_vals.ptr.p, state.ratio, state.scale))
+
+    # restore the input blob
+    copy!(inputs[1], state.etc[2])
   end
 end
