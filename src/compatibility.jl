480d96057db34f283135f4e9b4da4b09aeb80560
Chiyuan Zhang <pluskid@gmail.com>
2016-01-14 17:20:11 +0800

    fix compatability issue with v0.5 (#4)

--- src/compatibility.jl
+++ src/compatibility.jl
@@ -1,11 +1,18 @@
 using Compat
 
 if VERSION < v"0.3.3"
-  function blasfunc(name)
+  function blasfunc(name::Symbol)
     string(name)
   end
-else
-  function blasfunc(name)
+elseif VERSION < v"0.5.0"
+  function blasfunc(name::Symbol)
     Base.blasfunc(name)
+  end
+else
+  function blasfunc(name::Symbol)
+    str_name = string(name)
+    fnc_symb = eval(:(Base.@blasfunc $strname))
+    fnc_name = string(fnc_symb)
+    return fnc_name
   end
 end
