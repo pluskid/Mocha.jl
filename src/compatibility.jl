65abfbb858a85eb7ac2dde296f9cfc8e0c747be0
Michele Mastropietro <michele.mastropietro@gmail.com>
2016-01-26 00:41:52 +0100

    Fix typo and version number to check for blasfunc compatibility.
    blasfunc behavior has been changed in commit 57eb9c1e5cba37a522d82e131fde7b664b1aa9fe.
    Command `git rev-list v0.4.0..57eb9c1e5cba37a522d82e131fde7b664b1aa9fe --count` returns the numebr of commit since v0.4.0 which is 1915
    
    Typo prevented tests to be run.
    
    Shoud fix #4

--- src/compatibility.jl
+++ src/compatibility.jl
@@ -4,14 +4,14 @@ if VERSION < v"0.3.3"
   function blasfunc(name::Symbol)
     string(name)
   end
-elseif VERSION < v"0.5.0"
+elseif VERSION < v"0.5.0-dev+1915"
   function blasfunc(name::Symbol)
     Base.blasfunc(name)
   end
 else
   function blasfunc(name::Symbol)
     str_name = string(name)
-    fnc_symb = eval(:(Base.@blasfunc $strname))
+    fnc_symb = eval(:(Base.@blasfunc $str_name))
     fnc_name = string(fnc_symb)
     return fnc_name
   end
