c367c2020aa6be1c48979a019b481d611c6114df
rofinn <rory.finnegan@gmail.com>
2018-03-16 14:14:15 +0000

    Memento deprecation fixes.

--- src/logging.jl
+++ src/logging.jl
@@ -1,28 +1,28 @@
 using Memento
 export m_debug, m_info, m_notice, m_warn, m_error
 
-remove_handler(get_logger(Mocha), "console")
-add_handler(get_logger(Mocha),
-            DefaultHandler(STDOUT,
-                           DefaultFormatter("[{date} | {level} | {name}]: {msg}")))
-set_level(get_logger(Mocha), "info")
+# NOTE: It isn't generally recommended to configure your logger at the package/library level.
+push!(getlogger(Mocha),
+      DefaultHandler(STDOUT, DefaultFormatter("[{date} | {level} | {name}]: {msg}")))
+setlevel!(getlogger(Mocha), "info")
+setpropagating!(getlogger(Mocha), false)
 
 function m_debug(msg :: AbstractString...)
-  debug(get_logger(Mocha), prod(msg))
+  debug(getlogger(Mocha), prod(msg))
 end
 
 function m_info(msg :: AbstractString...)
-  info(get_logger(Mocha), prod(msg))
+  info(getlogger(Mocha), prod(msg))
 end
 
 function m_notice(msg :: AbstractString...)
-  notice(get_logger(Mocha), prod(msg))
+  notice(getlogger(Mocha), prod(msg))
 end
 
 function m_warn(msg :: AbstractString...)
-  warn(get_logger(Mocha), prod(msg))
+  warn(getlogger(Mocha), prod(msg))
 end
 
 function m_error(msg :: AbstractString...)
-  error(get_logger(Mocha), prod(msg))
+  error(getlogger(Mocha), prod(msg))
 end
